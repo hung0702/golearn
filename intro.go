38d77a76afccf98ecbea48a4511578eccd1128cd
hung0702 <Hung0702@gmail.com>
2024-04-01 17:39:11 -0400

    Added example text for file operations

--- intro.go
+++ intro.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 // "strconv"
 
@@ -277,13 +280,71 @@ Structures: arrays but allows different data types; can define objects
 Interfaces: collection of methods; can be used to define behavior.
 */
 
-func main() {
-	cali := kitten{name: "Cali", coat: "Calico", weight: "300g"}
-	fmt.Println(cali)
-}
+/*
+The animal interface allows for kitten and dog structs, which both have
+different variables and different weight units. For each struct, a specific
+metrictooz function will convert the weight based on known input unit. The
+printWeightInOz function only serves to consistently print the weight, easing
+future maintenance.
 
-type kitten struct {
-	name   string
-	coat   string
-	weight string
+Use PascalCase for public scope, camelCase for private scope.
+
+An enhanced implementation of this below function would use embedded struct for
+animal since name is repeated, and only specify unique variables for each struct
+(animal).
+*/
+
+// func main() {
+// 	cali := Kitten{Name: "Cali", Coat: "Calico", grams: 300}
+// 	imaginary := Dog{Name: "Spot", kilograms: 5}
+
+// 	fmt.Println(cali)
+// 	printWeightInOz(cali)
+
+// 	fmt.Println(imaginary)
+// 	printWeightInOz(imaginary)
+// }
+
+// type Animal interface {
+// 	metrictooz() float32
+// }
+
+// type Kitten struct {
+// 	Name  string
+// 	Coat  string
+// 	grams int
+// }
+
+// type Dog struct {
+// 	Name      string
+// 	kilograms int
+// }
+
+// func (k Kitten) metrictooz() float32 {
+// 	return float32(k.grams) * 0.03527396
+// }
+
+// func (d Dog) metrictooz() float32 {
+// 	return float32(d.kilograms) * 35.27396
+// }
+
+// func printWeightInOz(a Animal) {
+// 	fmt.Println("Weight in oz: ", a.metrictooz())
+// }
+
+/*
+File handling: read, write, create, delete, rename, etc.
+*/
+
+func main() {
+
+	file, err := os.Create("example.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.WriteString("Hi, I'm a file and I'm here to stay!")
+	file.Close()
+
 }
